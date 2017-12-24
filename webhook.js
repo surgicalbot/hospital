34bcf1fdae54e68d8dc9c7fb6b6e5f7cfb8fa1b7
srini23121987 <ursfriendly.sri23@gmail.com>
2017-12-25 01:30:54 +0530

    new commit

--- webhook.js
+++ webhook.js
@@ -184,6 +184,87 @@ let action = req.body.result.action; // https://dialogflow.com/docs/actions-and-
       db.close();
     });
   }
+  if(action=="hospital.operation.statistics"){
+      var hospitalarray = [];
+    const hospitaltype = parameters.hospital_type != '' ? parameters.hospital_type : "";
+    const treatmentyp = parameters.treatment_type != '' ? parameters.treatment_type : "";
+    const surgicaltyp = parameters.surgical_type != '' ? parameters.surgical_type : "";
+    const totalCost = (parameters.Statistics != "" && parameters.Statistics != null && parameters.Statistics != undefined) ? parameters.Statistics : "";
+    mongodb.MongoClient.connect("mongodb://admin:admin123@ds149335.mlab.com:49335/hospital", function (err, database) {
+      var db = database;
+      if (err) {
+        console.log(err);
+
+      }
+      filterarray = [
+        { $or: [{ "HOSPITAL": hospitaltype.toLowerCase() }, { "HOSPITAL": hospitaltype.toUpperCase() }, { "HOSPITAL": capitalizeFirstLetter(hospitaltype) }, { "HOSPITAL": toTitleCase(hospitaltype) }] },
+        { $or: [{ "TYPE": treatmentyp.toLowerCase() }, { "TYPE": treatmentyp.toUpperCase() }, { "TYPE": capitalizeFirstLetter(treatmentyp) }, { "TYPE": toTitleCase(treatmentyp) }] },
+        { $or: [{ "Operation": surgicaltyp.toLowerCase() }, { "Operation": surgicaltyp.toUpperCase() }, { "Operation": capitalizeFirstLetter(surgicaltyp) }, { "Operation": toTitleCase(surgicaltyp) }] },
+        { $or: [{ "Statistics": totalCost.toLowerCase() }, { "Statistics": totalCost.toUpperCase() }, { "Statistics": capitalizeFirstLetter(totalCost) }, { "Statistics": toTitleCase(totalCost) }] }
+      ]
+      db.collection("surgery").find({
+        $and: filterarray
+      }).toArray(function (err1, result1) {
+        if (err1) throw err1;
+        var html = '';
+        if (result1.length > 0) {
+          for (var key in result1[0]) {
+            if (key != '_id' && key.toLowerCase() != "date") {
+              html += `${key}: ${result1[0][key]}\n`;
+            }
+          }
+          if (html) {
+            db.collection("surgery").find({
+              $and: [{ $or: [{ "HOSPITAL": hospitaltype.toLowerCase() }, { "HOSPITAL": hospitaltype.toUpperCase() }, { "HOSPITAL": capitalizeFirstLetter(hospitaltype) }, { "HOSPITAL": toTitleCase(hospitaltype) }] }]
+            }).toArray(function (err2, result2) {
+            var finallarray = [];
+            var hospitalarray = [];
+            for (var keys in result) {
+             if (hospitalarray.indexOf(result[keys]["Statistics"]) < 0) {
+              if(result[keys]["Statistics"]){
+                hospitalarray.push(result[keys]["Statistics"]);
+              }
+              }
+            }
+            for (var treatsurgiment in hospitalarray) {
+              var html1 = {};
+              html1["title"] = hospitalarray[treatsurgiment];
+              html1["payload"] = hospitalarray[treatsurgiment];
+              html1["content_type"] = "text";
+              finallarray.push(html1);
+            }
+            html += "\ninterested in min/max/median case instead? or other hospital?";
+            res.json({
+              speech: "",
+              displayText: "",
+              source: 'agent',
+              "messages": [
+                {
+                  "type": 4,
+                  "platform": "facebook",
+                  "payload": {
+                    "facebook": {
+                      "text": html,
+                      "quick_replies": finallarray
+                    }
+                  }
+                }
+              ]
+            })
+          })
+          }
+        }
+        else 
+        {
+            res.status(200).json({
+              source: 'webhook',
+              speech: "I didnt get that",
+              displayText: "I didnt get that"
+            })
+          }
+      }); 
+    })
+  }
   if(action=="hospital.operation.options"){
     var hospitalarray = [];
     const hospitaltype = parameters.hospital_type != '' ? parameters.hospital_type : "";
