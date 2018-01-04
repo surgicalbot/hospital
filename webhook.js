ab4c82ce69badb41b6afef5ef2ece17bf9c57baa
botcheck <33388738+surgicalbot@users.noreply.github.com>
2018-01-04 08:55:32 +0530

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -122,6 +122,51 @@ let action = req.body.result.action; // https://dialogflow.com/docs/actions-and-
       db.close();
     });
   }
+  if(action == "input.hospitallist"){
+    mongodb.MongoClient.connect("mongodb://admin:admin123@ds149335.mlab.com:49335/hospital", function (err, database) {
+      var db = database;
+      if (err) {
+        console.log(err);
+      }
+      db.collection("surgery").find().toArray(function (err1, result1) {
+        var hospitalarray = [];
+        var finallarray=[];
+        for (var keys in result1) {
+         if (hospitalarray.indexOf(result1[keys]["HOSPITAL"]) < 0) {
+          if(result1[keys]["HOSPITAL"]){
+            hospitalarray.push(result1[keys]["HOSPITAL"]);
+          }
+          }
+        }
+        for (var treatsurgiment in hospitalarray) {
+          var html1 = {};
+          html1["title"] = hospitalarray[treatsurgiment];
+          html1["payload"] = hospitalarray[treatsurgiment];
+          html1["content_type"] = "text";
+          finallarray.push(html1);
+        }
+     
+        res.json({
+          speech: "",
+          displayText: "",
+          source: 'agent',
+          "messages": [
+            {
+              "type": 4,
+              "platform": "facebook",
+              "payload": {
+                "facebook": {
+                  "text": "Please Select the Hospital List",
+                  "quick_replies":finallarray
+                }
+              }
+            }
+          ]
+        })
+      });
+    });
+    
+  }
   if (action == "input.statistics") {
     var hospitalarray = [];
     const hospitaltype = parameters.hospital_type != '' ? parameters.hospital_type : "";
